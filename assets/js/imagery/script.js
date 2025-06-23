9591682a65aec3d0afb86e359391ef5935674b6f
DSTBP <TaddeoWang123@gmail.com>
2025-06-23 21:32:21 +0800

    Update

---
+++ assets/js/imagery/script.js
@@ -0,0 +1,11 @@
+// 加载动画自动隐藏
+window.addEventListener('DOMContentLoaded', function () {
+    const loader = document.querySelector('.loader');
+    if (loader) {
+        loader.style.transition = 'opacity 1s';
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 1000);
+    }
+}); 
